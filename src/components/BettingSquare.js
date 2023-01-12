1dfaf937de7b5fd064147c87ca4bb22a08e86670
Jed Duffey <jedduffey@gmail.com>
2023-01-12 18:28:38 -0500

    Refactor: use strings instead of numbers for bet names

--- src/components/BettingSquare.js
+++ src/components/BettingSquare.js
@@ -1,20 +1,20 @@
 export function BettingSquare(props) {
     const fontColor = "#FFFFFF";
-    const bgColor = getRouletteNumberColor(props.label);
+    const bgColor = getRouletteNumberColor(props.betName);
     const betAmountText = props.betAmount > 0 ?
         "$" + props.betAmount :
         String.fromCharCode(160); // non-breaking space
 
     return (
         <button
-            className={`betting-square-${props.label}`}
+            className={`betting-square-${props.betName}`}
             onClick={props.onClick}
             style={{
                 backgroundColor: bgColor,
                 color: fontColor
             }}
         >
-            <span className="square-label">{props.label}</span>
+            <span className="square-label">{props.betName}</span>
             <br />
             <br />
             <span className="square-bet-amount">{betAmountText}</span>
@@ -22,9 +22,7 @@ export function BettingSquare(props) {
     );
 }
 
-const getRouletteNumberColor = (i) => {
-    // TODO this will be replaced/refactored once we have all of the betting options on the board (e.g. even, odd)
-    const redNumbers = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
-    // const blackNumbers = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35];
-    return redNumbers.includes(i) ? "#d94848" : "#222222";
+const getRouletteNumberColor = (betName) => {
+    const redNumbers = ["1", "3", "5", "7", "9", "12", "14", "16", "18", "19", "21", "23", "25", "27", "30", "32", "34", "36"];
+    return redNumbers.includes(betName) ? "#d94848" : "#222222";
 }
--- src/components/Board.js
+++ src/components/Board.js
@@ -2,20 +2,19 @@ import React from 'react';
 
 import { BettingSquare } from "./BettingSquare";
 
-const TOP_ROW_STRAIGHT_UP = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36];
-const MIDDLE_ROW_STRAIGHT_UP = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35];
-const BOTTOM_ROW_STRAIGHT_UP = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34];
+const TOP_ROW_STRAIGHT_UP = ["3", "6", "9", "12", "15", "18", "21", "24", "27", "30", "33", "36"];
+const MIDDLE_ROW_STRAIGHT_UP = ["2", "5", "8", "11", "14", "17", "20", "23", "26", "29", "32", "35"];
+const BOTTOM_ROW_STRAIGHT_UP = ["1", "4", "7", "10", "13", "16", "19", "22", "25", "28", "31", "34"];
 
 export class Board extends React.Component {
-    renderSquare(i) {
-        console.log(i);
+    renderSquare(betName) {
         return (
             <BettingSquare
-                key={i}
-                onClick={() => this.props.onClick(i)}
-                label={i}
-                isSelected={this.props.bettingHistory.includes(i)}
-                betAmount={this.props.bettingHistory.filter((bet) => bet === i).length} // TODO replace this with key-value pair
+                key={betName}
+                onClick={() => this.props.onClick(betName)}
+                betName={betName}
+                isSelected={this.props.bettingHistory.includes(betName)}
+                betAmount={this.props.bettingHistory.filter((bet) => bet === betName).length} // TODO replace this with key-value pair
             />
         );
     }
@@ -23,9 +22,9 @@ export class Board extends React.Component {
     render() {
         return (
             <div className="game-board">
-                {TOP_ROW_STRAIGHT_UP.map((i) => this.renderSquare(i))}
-                {MIDDLE_ROW_STRAIGHT_UP.map((i) => this.renderSquare(i))}
-                {BOTTOM_ROW_STRAIGHT_UP.map((i) => this.renderSquare(i))}
+                {TOP_ROW_STRAIGHT_UP.map((betName) => this.renderSquare(betName))}
+                {MIDDLE_ROW_STRAIGHT_UP.map((betName) => this.renderSquare(betName))}
+                {BOTTOM_ROW_STRAIGHT_UP.map((betName) => this.renderSquare(betName))}
             </div>
         );
     }
