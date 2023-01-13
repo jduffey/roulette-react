d82a87987ba99bd57a1891ce0c479ca4729cd9b2
Jed Duffey <jedduffey@gmail.com>
2023-01-13 12:36:59 -0500

    Fix red/black colors of betting squares... and note to self to double-check CoPilot :-)

--- src/components/Game.js
+++ src/components/Game.js
@@ -58,6 +58,8 @@ export class Game extends React.Component {
     }
 
     getWheelNumberColor(wheelNumber) {
+        if (this.state.mostRecentSpinResult === null) { return "#dfdfdf"; }
+
         const redNumbers = new Set(["1", "3", "5", "7", "9", "12", "14", "16", "18", "19", "21", "23", "25", "27", "30", "32", "34", "36"]);
         const blackNumbers = new Set(["2", "4", "6", "8", "10", "11", "13", "15", "17", "20", "22", "24", "26", "28", "29", "31", "33", "35"]);
         const greenNumbers = new Set(["0", "00"]);
@@ -67,8 +69,6 @@ export class Game extends React.Component {
             return "#222222";
         } else if (greenNumbers.has(wheelNumber)) {
             return "#016D29";
-        } else {
-            return "rgb(223, 223, 223)";
         }
     }
 
