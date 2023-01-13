8041b7daaf58461c762b9c55c1dbba90a36a2890
Jed Duffey <jedduffey@gmail.com>
2023-01-13 13:10:38 -0500

    Clicking on 0/00 squares shows a chip with count of bet; WIP - swapping out other bet amount displays next, lots of cleanup/refactoring to do

--- src/components/BettingOptions/StraightUpZeroes.js
+++ src/components/BettingOptions/StraightUpZeroes.js
@@ -1,7 +1,6 @@
 export function StraightUpZeroes(props) {
-    const betAmountText = props.betAmount > 0 ?
-        "$" + props.betAmount :
-        String.fromCharCode(160); // non-breaking space
+    const betAmountText = "$" + props.betAmount;
+    const isVisible = props.betAmount > 0 ? "" : "none";
 
     return (
         <div
@@ -15,11 +14,18 @@ export function StraightUpZeroes(props) {
                 backgroundColor: props.styleData.backgroundColor,
             }}
         >
-            <div className="square-label">
-                {props.displayLabel}
-            </div>
-            <div className="square-bet-amount">
-                {betAmountText}
+            <div className="zero-square-contents">
+                <div className="zero-square-label">
+                    {props.displayLabel}
+                </div>
+                <div
+                    className="zero-square-chip"
+                    style={{
+                        display: isVisible
+                    }}
+                >
+                    {betAmountText}
+                </div>
             </div>
         </div>
     );
