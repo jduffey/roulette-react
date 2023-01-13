acf1da6acf1294eae02dd9fec980767c0eaade3d
Jed Duffey <jedduffey@gmail.com>
2023-01-12 20:47:58 -0500

    Wheel result box shows number color as background highlight

--- src/components/SpinResult.js
+++ src/components/SpinResult.js
@@ -1,7 +1,12 @@
 export function SpinResult(props) {
     return (
         <div className="spin-result-container">
-            <div className="spin-result-label">
+            <div
+                className="spin-result-label"
+                style={{
+                    backgroundColor: props.bgColor,
+                }}
+            >
                 {props.spinResult}
             </div>
         </div>
