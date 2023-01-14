298396764115ed809e36d348fa6d5afd9eb2e823
Jed Duffey <jedduffey@gmail.com>
2023-01-14 11:50:53 -0500

    Pass betting square text label to component function

--- src/components/Board.js
+++ src/components/Board.js
@@ -12,9 +12,12 @@ const convertBettingSquareDataToObject = (bettingSquareData) => {
             ...acc,
             [next[0]]:
             {
-                left: next[1],
-                top: next[2],
-                backgroundColor: next[3],
+                styleData: {
+                    left: next[1],
+                    top: next[2],
+                    backgroundColor: next[3],
+                },
+                displayText: next[4],
             }
         };
     }, {});
@@ -22,70 +25,70 @@ const convertBettingSquareDataToObject = (bettingSquareData) => {
 
 const BETTING_SQUARES_STRAIGHT_UP_1_36 = convertBettingSquareDataToObject([
     // Top row
-    ["3", 80, 0, "#d94848"],
-    ["6", 160, 0, "#222222"],
-    ["9", 240, 0, "#d94848"],
-    ["12", 320, 0, "#d94848"],
-    ["15", 400, 0, "#222222"],
-    ["18", 480, 0, "#d94848"],
-    ["21", 560, 0, "#d94848"],
-    ["24", 640, 0, "#222222"],
-    ["27", 720, 0, "#d94848"],
-    ["30", 800, 0, "#d94848"],
-    ["33", 880, 0, "#222222"],
-    ["36", 960, 0, "#d94848"],
+    ["3", 80, 0, "#d94848", "3"],
+    ["6", 160, 0, "#222222", "6"],
+    ["9", 240, 0, "#d94848", "9"],
+    ["12", 320, 0, "#d94848", "12"],
+    ["15", 400, 0, "#222222", "15"],
+    ["18", 480, 0, "#d94848", "18"],
+    ["21", 560, 0, "#d94848", "21"],
+    ["24", 640, 0, "#222222", "24"],
+    ["27", 720, 0, "#d94848", "27"],
+    ["30", 800, 0, "#d94848", "30"],
+    ["33", 880, 0, "#222222", "33"],
+    ["36", 960, 0, "#d94848", "36"],
     // Middle row
-    ["2", 80, 120, "#222222"],
-    ["5", 160, 120, "#d94848"],
-    ["8", 240, 120, "#222222"],
-    ["11", 320, 120, "#222222"],
-    ["14", 400, 120, "#d94848"],
-    ["17", 480, 120, "#222222"],
-    ["20", 560, 120, "#222222"],
-    ["23", 640, 120, "#d94848"],
-    ["26", 720, 120, "#222222"],
-    ["29", 800, 120, "#222222"],
-    ["32", 880, 120, "#d94848"],
-    ["35", 960, 120, "#222222"],
+    ["2", 80, 120, "#222222", "2"],
+    ["5", 160, 120, "#d94848", "5"],
+    ["8", 240, 120, "#222222", "8"],
+    ["11", 320, 120, "#222222", "11"],
+    ["14", 400, 120, "#d94848", "14"],
+    ["17", 480, 120, "#222222", "17"],
+    ["20", 560, 120, "#222222", "20"],
+    ["23", 640, 120, "#d94848", "23"],
+    ["26", 720, 120, "#222222", "26"],
+    ["29", 800, 120, "#222222", "29"],
+    ["32", 880, 120, "#d94848", "32"],
+    ["35", 960, 120, "#222222", "35"],
     // Bottom row
-    ["1", 80, 240, "#d94848"],
-    ["4", 160, 240, "#222222"],
-    ["7", 240, 240, "#d94848"],
-    ["10", 320, 240, "#222222"],
-    ["13", 400, 240, "#222222"],
-    ["16", 480, 240, "#d94848"],
-    ["19", 560, 240, "#d94848"],
-    ["22", 640, 240, "#222222"],
-    ["25", 720, 240, "#d94848"],
-    ["28", 800, 240, "#222222"],
-    ["31", 880, 240, "#222222"],
-    ["34", 960, 240, "#d94848"],
+    ["1", 80, 240, "#d94848", "1"],
+    ["4", 160, 240, "#222222", "4"],
+    ["7", 240, 240, "#d94848", "7"],
+    ["10", 320, 240, "#222222", "10"],
+    ["13", 400, 240, "#222222", "13"],
+    ["16", 480, 240, "#d94848", "16"],
+    ["19", 560, 240, "#d94848", "19"],
+    ["22", 640, 240, "#222222", "22"],
+    ["25", 720, 240, "#d94848", "25"],
+    ["28", 800, 240, "#222222", "28"],
+    ["31", 880, 240, "#222222", "31"],
+    ["34", 960, 240, "#d94848", "34"],
 ]);
 
 const BETTING_SQUARES_STRAIGHT_UP_ZEROES = convertBettingSquareDataToObject([
-    ["0", 0, 180, "#016D29"],
-    ["00", 0, 0, "#016D29"],
+    ["0", 0, 180, "#016D29", "0"],
+    ["00", 0, 0, "#016D29", "00"],
 ]);
 
 const BETTING_SQUARES_DOZENS = convertBettingSquareDataToObject([
-    ["1st 12", 80, 360, "#016D29"],
-    ["2nd 12", 400, 360, "#016D29"],
-    ["3rd 12", 720, 360, "#016D29"],
+    ["1st 12", 80, 360, "#016D29", "1st 12"],
+    ["2nd 12", 400, 360, "#016D29", "2nd 12"],
+    ["3rd 12", 720, 360, "#016D29", "3rd 12"],
 ]);
 
 const BETTING_SQUARES_ROWS = convertBettingSquareDataToObject([
-    ["Top", 1040, 0, "#016D29"],
-    ["Middle", 1040, 120, "#016D29"],
-    ["Bottom", 1040, 240, "#016D29"],
+    ["Top", 1040, 0, "#016D29", "2 to 1"],
+    ["Middle", 1040, 120, "#016D29", "2 to 1"],
+    ["Bottom", 1040, 240, "#016D29", "2 to 1"],
 ]);
 
 const BETTING_SQUARES_HALVES = convertBettingSquareDataToObject([
-    ["1 to 18", 80, 440, "#016D29"],
-    ["Even", 240, 440, "#016D29"],
-    ["Red", 400, 440, "#d94848"],
-    ["Black", 560, 440, "#222222"],
-    ["Odd", 720, 440, "#016D29"],
-    ["19 to 36", 880, 440, "#016D29"],
+    ["1 to 18", 80, 440, "#016D29", "1 to 18"],
+    ["Even", 240, 440, "#016D29", "Even"],
+    ["Red", 400, 440, "#d94848", "Red"],
+    ["Black", 560, 440, "#222222", "Black"],
+    ["Odd", 720, 440, "#016D29", "Odd"],
+    ["19 to 36", 880, 440, "#016D29", "19 to 36"],
 ]);
 
 const bettingSquares1To36Names = Object.keys(BETTING_SQUARES_STRAIGHT_UP_1_36);
@@ -95,62 +98,62 @@ const bettingSquareRowsNames = Object.keys(BETTING_SQUARES_ROWS);
 const bettingSquareHalvesNames = Object.keys(BETTING_SQUARES_HALVES);
 
 export class Board extends React.Component {
-    render1To36Squares(betName, styleData) {
+    render1To36Squares(betName, displayData) {
         return (
             <StraightUp1To36
                 key={betName}
                 onClick={() => this.props.onClick(betName)}
-                displayLabel={betName}
+                displayLabel={displayData.displayText}
                 betAmount={this.props.bettingHistory[betName]}
-                styleData={styleData}
+                styleData={displayData.styleData}
             />
         );
     }
 
-    renderZeroesSquares(betName, styleData) {
+    renderZeroesSquares(betName, displayData) {
         return (
             <StraightUpZeroes
                 key={betName}
                 onClick={() => this.props.onClick(betName)}
-                displayLabel={betName}
+                displayLabel={displayData.displayText}
                 betAmount={this.props.bettingHistory[betName]}
-                styleData={styleData}
+                styleData={displayData.styleData}
             />
         );
     }
 
-    renderDozensSquares(betName, styleData) {
+    renderDozensSquares(betName, displayData) {
         return (
             <Dozens
                 key={betName}
                 onClick={() => this.props.onClick(betName)}
-                displayLabel={betName}
+                displayLabel={displayData.displayText}
                 betAmount={this.props.bettingHistory[betName]}
-                styleData={styleData}
+                styleData={displayData.styleData}
             />
         );
     }
 
-    renderRowsSquares(betName, styleData) {
+    renderRowsSquares(betName, displayData) {
         return (
             <Rows
                 key={betName}
                 onClick={() => this.props.onClick(betName)}
-                displayLabel={"2 to 1"}
+                displayLabel={displayData.displayText}
                 betAmount={this.props.bettingHistory[betName]}
-                styleData={styleData}
+                styleData={displayData.styleData}
             />
         );
     }
 
-    renderHalvesSquares(betName, styleData) {
+    renderHalvesSquares(betName, displayData) {
         return (
             <Halves
                 key={betName}
                 onClick={() => this.props.onClick(betName)}
-                displayLabel={betName}
+                displayLabel={displayData.displayText}
                 betAmount={this.props.bettingHistory[betName]}
-                styleData={styleData}
+                styleData={displayData.styleData}
             />
         );
     }
