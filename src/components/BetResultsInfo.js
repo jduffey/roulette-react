0663844a23816acc62ee7f65cdbf3f4bac0f93c7
Jed Duffey <jedduffey@gmail.com>
2023-01-21 22:18:50 -0500

    Fill out BET_NAMES const values and implement method for getting bet name mulitplier

--- src/components/BetResultsInfo.js
+++ src/components/BetResultsInfo.js
@@ -1,3 +1,4 @@
+import { getBetNameMultiplier } from "../common/getBetNameMultiplier";
 import { getWinningCriteria } from "../common/getWinningCriteria";
 
 const className = "bet-results-info";
@@ -105,63 +106,12 @@ export function BetResultsInfo(props) {
 }
 
 function calculateWinningsOnBet(props, betOption) {
-    const multipliers = {
-        "StraightUp_0": 35,
-        "StraightUp_00": 35,
-        "StraightUp_1": 35,
-        "StraightUp_2": 35,
-        "StraightUp_3": 35,
-        "StraightUp_4": 35,
-        "StraightUp_5": 35,
-        "StraightUp_6": 35,
-        "StraightUp_7": 35,
-        "StraightUp_8": 35,
-        "StraightUp_9": 35,
-        "StraightUp_10": 35,
-        "StraightUp_11": 35,
-        "StraightUp_12": 35,
-        "StraightUp_13": 35,
-        "StraightUp_14": 35,
-        "StraightUp_15": 35,
-        "StraightUp_16": 35,
-        "StraightUp_17": 35,
-        "StraightUp_18": 35,
-        "StraightUp_19": 35,
-        "StraightUp_20": 35,
-        "StraightUp_21": 35,
-        "StraightUp_22": 35,
-        "StraightUp_23": 35,
-        "StraightUp_24": 35,
-        "StraightUp_25": 35,
-        "StraightUp_26": 35,
-        "StraightUp_27": 35,
-        "StraightUp_28": 35,
-        "StraightUp_29": 35,
-        "StraightUp_30": 35,
-        "StraightUp_31": 35,
-        "StraightUp_32": 35,
-        "StraightUp_33": 35,
-        "StraightUp_34": 35,
-        "StraightUp_35": 35,
-        "StraightUp_36": 35,
-        "1st 12": 2,
-        "2nd 12": 2,
-        "3rd 12": 2,
-        "1st Column": 2,
-        "2nd Column": 2,
-        "3rd Column": 2,
-        "1 to 18": 1,
-        "19 to 36": 1,
-        "Even": 1,
-        "Odd": 1,
-        "Red": 1,
-        "Black": 1,
-    };
+    const multiplier = getBetNameMultiplier(betOption);
 
     const betAmount = props.bets[betOption];
 
     return getWinningCriteria(props.winningWheelNumber).includes(betOption) ?
-        betAmount * multipliers[betOption] :
+        betAmount * multiplier :
         0;
 }
 
--- src/components/Game.js
+++ src/components/Game.js
@@ -9,6 +9,7 @@ import { PlayerInfo } from './PlayerInfo';
 import { SpinButton } from './SpinButton';
 import { SpinResult } from './SpinResult';
 
+import { getBetNameMultiplier } from '../common/getBetNameMultiplier';
 import { getRandomWheelNumber } from '../common/getRandomWheelNumber';
 import { getWinningCriteria } from '../common/getWinningCriteria';
 
@@ -140,70 +141,19 @@ export class Game extends React.Component {
     }
 
     calculateWinningsPlusReturnedBets(betsOnBoard, winningBetOptions) {
-        const winnings = Object.keys(betsOnBoard).reduce((acc, betOption) => {
-            const multipliers = {
-                "StraightUp_1": 35,
-                "StraightUp_2": 35,
-                "StraightUp_3": 35,
-                "StraightUp_4": 35,
-                "StraightUp_5": 35,
-                "StraightUp_6": 35,
-                "StraightUp_7": 35,
-                "StraightUp_8": 35,
-                "StraightUp_9": 35,
-                "StraightUp_10": 35,
-                "StraightUp_11": 35,
-                "StraightUp_12": 35,
-                "StraightUp_13": 35,
-                "StraightUp_14": 35,
-                "StraightUp_15": 35,
-                "StraightUp_16": 35,
-                "StraightUp_17": 35,
-                "StraightUp_18": 35,
-                "StraightUp_19": 35,
-                "StraightUp_20": 35,
-                "StraightUp_21": 35,
-                "StraightUp_22": 35,
-                "StraightUp_23": 35,
-                "StraightUp_24": 35,
-                "StraightUp_25": 35,
-                "StraightUp_26": 35,
-                "StraightUp_27": 35,
-                "StraightUp_28": 35,
-                "StraightUp_29": 35,
-                "StraightUp_30": 35,
-                "StraightUp_31": 35,
-                "StraightUp_32": 35,
-                "StraightUp_33": 35,
-                "StraightUp_34": 35,
-                "StraightUp_35": 35,
-                "StraightUp_36": 35,
-                "StraightUp_0": 35,
-                "StraightUp_00": 35,
-                "1st 12": 2,
-                "2nd 12": 2,
-                "3rd 12": 2,
-                "1st Column": 2,
-                "2nd Column": 2,
-                "3rd Column": 2,
-                "1 to 18": 1,
-                "19 to 36": 1,
-                "Even": 1,
-                "Odd": 1,
-                "Red": 1,
-                "Black": 1,
-            };
+        const winnings = Object.keys(betsOnBoard).reduce((acc, betName) => {
+            const multiplier = getBetNameMultiplier(betName);
 
             return acc +
-                (winningBetOptions.includes(betOption) ?
-                    betsOnBoard[betOption] * multipliers[betOption] :
+                (winningBetOptions.includes(betName) ?
+                    betsOnBoard[betName] * multiplier :
                     0);
         }, 0);
 
-        const betsReturned = Object.keys(betsOnBoard).reduce((acc, betOption) => {
+        const betsReturned = Object.keys(betsOnBoard).reduce((acc, betName) => {
             return acc +
-                (winningBetOptions.includes(betOption) ?
-                    betsOnBoard[betOption] :
+                (winningBetOptions.includes(betName) ?
+                    betsOnBoard[betName] :
                     0);
         }, 0);
 
