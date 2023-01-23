5e31fd40d8ef339755e8bc0d6ce484b70d75c7a7
Jed Duffey <jedduffey@gmail.com>
2023-01-23 14:54:35 -0500

    Create snapshot tests for Chip component

---
+++ src/test/Chip.test.js
@@ -0,0 +1,66 @@
+import renderer from 'react-test-renderer';
+
+import { Chip } from '../components/Chip';
+
+describe('Chip', () => {
+    describe('boundary tests for chip colors', () => {
+        const boundaryValues = [
+            [0],
+            [1],
+            [4],
+            [5],
+            [24],
+            [25],
+            [99],
+            [100],
+            [499],
+            [500],
+            [999],
+            [1000],
+        ];
+        const id = "ID";
+        const key = "KEY";
+        const className = "CLASSNAME";
+        const onClick = jest.fn();
+
+        describe('when isSelected is false', () => {
+            it.each(boundaryValues)('chip amount %s', (chipAmount) => {
+                const isSelected = false;
+
+                const sut =
+                    Chip({
+                        id: id,
+                        key: key,
+                        className: className,
+                        chipAmount: chipAmount,
+                        onClick: onClick,
+                        isSelected: isSelected,
+                    });
+
+                const actual = renderer.create(sut);
+
+                expect(actual).toMatchSnapshot();
+            });
+        });
+
+        describe('when isSelected is true', () => {
+            it.each(boundaryValues)('chip amount %s', (chipAmount) => {
+                const isSelected = true;
+
+                const sut =
+                    Chip({
+                        id: id,
+                        key: key,
+                        className: className,
+                        chipAmount: chipAmount,
+                        onClick: onClick,
+                        isSelected: isSelected,
+                    });
+
+                const actual = renderer.create(sut);
+
+                expect(actual).toMatchSnapshot();
+            });
+        });
+    });
+});
