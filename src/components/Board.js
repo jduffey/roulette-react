5587dafa8754efa5b6e1c14691ce1a30227cd31d
Jed Duffey <jedduffey@gmail.com>
2023-01-23 22:16:34 -0500

    Finish extracting/consolidating bet option parameters

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,40 +2,23 @@ import React from 'react';
 
 import { BetOption } from './BetOption';
 
-import {
-    bettingSquares1To36Names,
-    bettingSquareStraightUpZeroesNames,
-    bettingSquareDozensNames,
-    bettingSquareColumnNames,
-    bettingSquareHalvesNames,
-    BETTING_SQUARES_STRAIGHT_UP_1_36,
-    BETTING_SQUARES_STRAIGHT_UP_ZEROES,
-    BETTING_SQUARES_DOZENS,
-    BETTING_SQUARES_COLUMNS,
-    BETTING_SQUARES_HALVES,
-} from './betOptionParameters';
+import { BET_OPTION_PARAMS } from './betOptionParameters';
 
 export function Board(props) {
-    function renderBetOption(betName, displayData, classNamePrefix) {
-        return (
-            <BetOption
-                key={betName}
-                onClick={() => props.onClick(betName)}
-                displayText={displayData.displayText}
-                betAmount={props.betsOnBoard[betName]}
-                styleData={displayData.styleData}
-                classNamePrefix={classNamePrefix}
-            />
-        );
-    }
-
     return (
         <div className="game-board">
-            {bettingSquares1To36Names.map((betName) => renderBetOption(betName, BETTING_SQUARES_STRAIGHT_UP_1_36[betName], "straightUp1to36"))}
-            {bettingSquareStraightUpZeroesNames.map((betName) => renderBetOption(betName, BETTING_SQUARES_STRAIGHT_UP_ZEROES[betName], "zero"))}
-            {bettingSquareColumnNames.map((betName) => renderBetOption(betName, BETTING_SQUARES_COLUMNS[betName], "column"))}
-            {bettingSquareDozensNames.map((betName) => renderBetOption(betName, BETTING_SQUARES_DOZENS[betName], "dozens"))}
-            {bettingSquareHalvesNames.map((betName) => renderBetOption(betName, BETTING_SQUARES_HALVES[betName], "halves"))}
+            {Object.entries(BET_OPTION_PARAMS).map(([betOptionName, params]) => {
+                return (
+                    <BetOption
+                        key={betOptionName}
+                        onClick={() => props.onClick(betOptionName)}
+                        displayText={params.displayText}
+                        betAmount={props.betsOnBoard[betOptionName]}
+                        styleData={params.styleData}
+                        classNamePrefix={params.classNamePrefix}
+                    />
+                );
+            })}
         </div>
     );
 }
