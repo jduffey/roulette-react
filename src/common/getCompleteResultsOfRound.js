07d71c354fe41a014a727a7d63d32c47422c393e
Jed Duffey <jedduffey@gmail.com>
2023-01-30 19:54:59 -0500

    Refactor getCompleteResultsOfRound and delete getResultsOfBets

--- src/common/getCompleteResultsOfRound.js
+++ src/common/getCompleteResultsOfRound.js
@@ -1,20 +1,35 @@
-import { getResultsOfBets } from './getResultsOfBets';
+import { getBetNameMultiplier } from "./getBetNameMultiplier";
+import { getWinningCriteria } from "./getWinningCriteria";
 
 export const getCompleteResultsOfRound = (startingBalance, bets, winningWheelNumber) => {
-    const totalBetAmount = Object.values(bets).reduce((acc, currentValue) => acc + currentValue, 0);
+    const winningCriteria = getWinningCriteria(winningWheelNumber);
 
-    const resultsOfBets = getResultsOfBets(bets, winningWheelNumber);
+    const totalBetAmount =
+        Object.values(bets).reduce((acc, currentValue) => acc + currentValue, 0);
 
-    const totalWinningsFromBets = Object.values(resultsOfBets).reduce((acc, betResult) => acc + betResult.winningsOnBet, 0);
-    const totalBetAmountReturned = Object.values(resultsOfBets).reduce((acc, betResult) => acc + betResult.betReturned, 0);
+    const resultsOfBets =
+        Object.entries(bets).reduce((acc, [betName, betAmount]) => {
+            acc[betName] = {
+                betAmount: betAmount,
+                winningsOnBet: winningCriteria.has(betName) ? betAmount * getBetNameMultiplier(betName) : 0,
+                betReturned: winningCriteria.has(betName) ? betAmount : 0,
+            };
+            return acc;
+        }, {});
+
+    const totalWinningsFromBets =
+        Object.values(resultsOfBets).reduce((acc, betResult) => acc + betResult.winningsOnBet, 0);
+
+    const totalBetAmountReturned =
+        Object.values(resultsOfBets).reduce((acc, betResult) => acc + betResult.betReturned, 0);
 
     const finalBalance =
         startingBalance - totalBetAmount + totalWinningsFromBets + totalBetAmountReturned;
 
     return {
         startingBalance: startingBalance,
-        finalBalance: finalBalance,
         betsPlaced: resultsOfBets,
         winningWheelNumber: winningWheelNumber,
+        finalBalance: finalBalance,
     };
 }
