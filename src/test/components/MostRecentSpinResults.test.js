b532c4ced6b75e3f1adb6e5007d408917be8c1a9
Jed Duffey <jedduffey@gmail.com>
2023-01-31 20:28:54 -0500

    Add test for MostRecentSpinResults for when there are more spin results than will be displayed

--- src/test/components/MostRecentSpinResults.test.js
+++ src/test/components/MostRecentSpinResults.test.js
@@ -6,6 +6,7 @@ describe('MostRecentSpinResults', () => {
     it.each([
         ["no spin results", []],
         ["multiple spin results", ["00", "1", "2"]],
+        ["more spin results than will be displayed", ["0", "00", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]],
     ])('renders when there are %s', (_description, spinResults) => {
         const sut =
             MostRecentSpinResults({
