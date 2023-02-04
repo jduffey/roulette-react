f97184131854978ac25523d2378ee627bed9c32f
Jed Duffey <jedduffey@gmail.com>
2023-02-04 16:57:53 -0500

    Minor cleanup and temporary styling to NavigationBar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,12 @@
-import { Layout } from "./Layout";
-
 export function Home() {
     return (
-            <div
-                style={{
-                    color: "white",
-                    fontSize: "4rem",
-                }}
-            >
-                PLACEHOLDER PAGE FOR NEXT GAME
-            </div>
+        <div
+            style={{
+                color: "white",
+                fontSize: "4rem",
+            }}
+        >
+            PLACEHOLDER PAGE FOR HOME
+        </div>
     )
 }
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export function NavigationBar() {
     return (
         <div className="NavigationBar-component">
-            <div className="NavigationBar-component-title">
+            <div className="NavigationBar-links">
                 <Link to="/">Home</Link>
                 <Link to="/roulette">Roulette</Link>
                 <Link to="/next-game">Next Game</Link>
