7046203cfb672e543442fe8d9f0dfb9d7cde9942
Jed Duffey <jedduffey@gmail.com>
2023-02-09 12:47:01 -0500

    DeepSource fix: JSX max depth

--- src/components/roulette/PendingBetsTable.js
+++ src/components/roulette/PendingBetsTable.js
@@ -13,28 +13,28 @@ const betRow = (betOption, betAmount) => {
 
 const CLASS_NAME = "PendingBetsTable-component";
 export function PendingBetsTable(props) {
+    const tableBody = (
+        <tbody>
+            <tr style={{
+                textAlign: "center",
+            }}>
+                <th>Bet Name</th>
+                <th>Bet Amount</th>
+            </tr>
+            {Object.entries(props.pendingBets.reduce((acc, pendingBet) => {
+                acc[pendingBet.betName] = (acc[pendingBet.betName] || 0) + pendingBet.betAmount;
+                return acc;
+            }, {})).map(([betOption, betAmount]) => betRow(betOption, betAmount))}
+        </tbody>
+    )
+
     return (
-        <div
-            className={CLASS_NAME}
-        >
-            <div
-                className="pending-bets-info-table-title-text"
-            >
+        <div className={CLASS_NAME}>
+            <div className="pending-bets-info-table-title-text">
                 Pending Bets
             </div>
             <table className="pending-bets-info-table">
-                <tbody>
-                    <tr style={{
-                        textAlign: "center",
-                    }}>
-                        <th>Bet Name</th>
-                        <th>Bet Amount</th>
-                    </tr>
-                    {Object.entries(props.pendingBets.reduce((acc, pendingBet) => {
-                        acc[pendingBet.betName] = (acc[pendingBet.betName] || 0) + pendingBet.betAmount;
-                        return acc;
-                    }, {})).map(([betOption, betAmount]) => betRow(betOption, betAmount))}
-                </tbody>
+                {tableBody}
             </table>
         </div>
     );
