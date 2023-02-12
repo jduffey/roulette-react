21d79c679258f4f19ed3e994d6ec94c6867835ac
Jed Duffey <jedduffey@gmail.com>
2023-02-11 22:13:06 -0500

    Rename betOption -> betName in roulette files

--- src/components/roulette/Board.js
+++ src/components/roulette/Board.js
@@ -8,14 +8,14 @@ export function Board(props) {
         <div
             className={CLASS_NAME}
         >
-            {Object.entries(DISPLAY_PARAMS).map(([betOptionName, params]) => {
+            {Object.entries(DISPLAY_PARAMS).map(([betName, params]) => {
                 return (
                     <ClickableBet
                         textLabelClassNamePrefix={params.textLabelClassNamePrefix}
-                        key={betOptionName}
-                        onClick={() => props.onClick(betOptionName)}
+                        key={betName}
+                        onClick={() => props.onClick(betName)}
                         displayText={params.displayText}
-                        betAmount={props.pendingBets.filter(pendingBet => pendingBet.betName === betOptionName).reduce((acc, pendingBet) => acc + pendingBet.betAmount, 0)}
+                        betAmount={props.pendingBets.filter(pendingBet => pendingBet.betName === betName).reduce((acc, pendingBet) => acc + pendingBet.betAmount, 0)}
                         styleData={params.styleData}
                     />
                 );
