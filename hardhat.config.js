0d588e30e36a856addb99b8bbdbe398d3d9762ef
Jed Duffey <jedduffey@gmail.com>
2023-02-17 13:27:24 -0500

    Generate only 5 accounts (code needs updated to account for this.. no pun intended)

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,12 @@ module.exports = {
     solidity: "0.8.17",
     networks: {
         hardhat: {
-            chainId: 1337 // We set 1337 to make interacting with MetaMask simpler
-        }
-    }
+            chainId: 1337, // From hardhat-boilerplate tutorial: setting to 1337 makes interacting with MetaMask simpler
+            accounts: {
+                count: 5, // default: 20
+                initialIndex: 0, // default: 0
+                accountsBalance: "10000000000000000000000", // default: 10000000000000000000000 (wei, or 10,000 ETH)
+            },
+        },
+    },
 };
