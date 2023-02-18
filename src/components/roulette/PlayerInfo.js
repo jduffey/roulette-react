f7f946804bf291cfe53d9638b0ba7b36bc59e5f7
Jed Duffey <jedduffey@gmail.com>
2023-02-18 08:26:49 -0500

    WIP: simulate playing the game

--- src/components/roulette/PlayerInfo.js
+++ src/components/roulette/PlayerInfo.js
@@ -16,18 +16,20 @@ export function PlayerInfo(props) {
     const [tokenBalance, setTokenBalance] = useState("Not loaded...");
 
     // Adapted from https://github.com/ethers-io/ethers.js/issues/1053#issuecomment-808736570
-    const EXPECTED_PONG_BACK = 2000
-    const KEEP_ALIVE_CHECK_INTERVAL = 1000
+    const EXPECTED_PONG_BACK = 1000
+    const KEEP_ALIVE_CHECK_INTERVAL = 455
 
     let startCounter = 1;
     const startConnection = () => {
         if (typeof wsProvider._websocket === "undefined") {
-            console.log("Websocket connection is undefined. Is the blockchain node running?");
+            // console.log("Websocket connection is undefined. Is the blockchain node running?");
             return;
         }
 
         // TODO why does this counter always make it to 2 (twice)?
-        console.log(`Websocket connection started ${startCounter++} time(s).`);
+        // Look in Chrome dev tools Network tab and see that the websocket-provider always takes
+        // EXPECTED_PONG_BACK + KEEP_ALIVE_CHECK_INTERVAL to respond.
+        // console.log(`Websocket connection started ${startCounter++} time(s).`);
 
         wsProvider._websocket.onmessage = (event) => {
             console.log("An event was received:", event);
@@ -36,24 +38,24 @@ export function PlayerInfo(props) {
         let pingTimeout = null
         let keepAliveInterval = null
         wsProvider._websocket.onopen = () => {
-            console.log("onopen");
+            // console.log("onopen");
             keepAliveInterval = setInterval(() => {
-                console.log("Running the interval", Date.now());
+                // console.log("Running the interval", Date.now());
                 (async (addr) => {
-                    console.log("Getting token balance", Date.now());
+                    // console.log("Getting token balance", Date.now());
                     const balance = await getTokenBalance(addr);
                     const balanceAsNumber = Number(balance);
                     setTokenBalance(balanceAsNumber);
                 })(FIRST_PLAYER_ADDRESS);
                 pingTimeout = setTimeout(() => {
-                    console.log("setting what happens if the ping times out", Date.now());
+                    // console.log("setting what happens if the ping times out", Date.now());
                     wsProvider._websocket.close();
                 }, EXPECTED_PONG_BACK)
             }, KEEP_ALIVE_CHECK_INTERVAL)
         }
 
         wsProvider._websocket.onclose = () => {
-            console.log("onclose");
+            // console.log("onclose");
             clearInterval(keepAliveInterval);
             clearTimeout(pingTimeout);
             startConnection();
--- src/components/roulette/Roulette.js
+++ src/components/roulette/Roulette.js
@@ -24,6 +24,8 @@ import { CompletionsCounter } from './CompletionsCounter';
 
 import { transferFrom, FIRST_PLAYER_ADDRESS, HOUSE_ADDRESS } from '../../common/blockchainWrapper';
 
+import { simulatePlayingGame } from '../../common/simulatePlayingGame';
+
 function calculateTotalBetAmount(bets) {
     return bets.reduce((acc, pendingBet) => acc + pendingBet.betAmount, 0);
 }
