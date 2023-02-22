81a8ac9be55887203a6b76daa8650703af7a05e8
Jed Duffey <jedduffey@gmail.com>
2023-02-21 21:37:35 -0500

    Boundary tests for Overlay; need to pare down non-overlay tests

--- src/test/components/roulette/NumbersHitGameCounter.test.js
+++ src/test/components/roulette/NumbersHitGameCounter.test.js
@@ -4,7 +4,7 @@ import { NumbersHitGameCounter } from "../../../components/roulette/NumberHitGam
 import { NumbersHitGameCounterOverlay } from "../../../components/roulette/NumberHitGameCounter";
 
 describe("NumbersHitGameCounter", () => {
-    it('renders when there are no spins', () => {
+    it('all boxes inactive when there are no spins', () => {
         const sut = NumbersHitGameCounter({
             transactionHistory: [],
         });
@@ -14,7 +14,7 @@ describe("NumbersHitGameCounter", () => {
         expect(view).toMatchSnapshot();
     });
 
-    it('renders when there is 1 spin', () => {
+    it('1st box only is active when there is 1 spin', () => {
         const sut = NumbersHitGameCounter({
             transactionHistory: [['x']],
         });
@@ -24,7 +24,7 @@ describe("NumbersHitGameCounter", () => {
         expect(view).toMatchSnapshot();
     });
 
-    it('renders when there are 39 spins', () => {
+    it('1st box only is active when there are 39 spins', () => {
         const sut = NumbersHitGameCounter({
             transactionHistory: Array(39).fill(['x']),
         });
@@ -34,7 +34,7 @@ describe("NumbersHitGameCounter", () => {
         expect(view).toMatchSnapshot();
     });
 
-    it('renders when there are 40 spins', () => {
+    it('2nd box is active when there are 40 spins', () => {
         const sut = NumbersHitGameCounter({
             transactionHistory: Array(40).fill(['x']),
         });
@@ -44,7 +44,7 @@ describe("NumbersHitGameCounter", () => {
         expect(view).toMatchSnapshot();
     });
 
-    it('renders when there are 49 spins', () => {
+    it('2nd box is active when there are 49 spins', () => {
         const sut = NumbersHitGameCounter({
             transactionHistory: Array(49).fill(['x']),
         });
@@ -54,7 +54,7 @@ describe("NumbersHitGameCounter", () => {
         expect(view).toMatchSnapshot();
     });
 
-    it('renders when there are 50 spins', () => {
+    it('3rd box is active when there are 50 spins', () => {
         const sut = NumbersHitGameCounter({
             transactionHistory: Array(50).fill(['x']),
         });
@@ -65,9 +65,64 @@ describe("NumbersHitGameCounter", () => {
     });
 });
 
-// describe("NumbersHitGameCounterOverlay", () => {
-//     it("renders", () => {
-//         const wrapper = shallow(<NumbersHitGameCounterOverlay transactionHistory={[]} />);
-//         expect(wrapper.exists()).toBe(true);
-//     });
-// });
+describe("NumbersHitGameCounterOverlay", () => {
+    it('all boxes inactive when there are no spins', () => {
+        const sut = NumbersHitGameCounterOverlay({
+            transactionHistory: [],
+        });
+
+        const view = renderer.create(sut);
+
+        expect(view).toMatchSnapshot();
+    });
+
+    it('1st box only is active when there is 1 spin', () => {
+        const sut = NumbersHitGameCounterOverlay({
+            transactionHistory: [['x']],
+        });
+
+        const view = renderer.create(sut);
+
+        expect(view).toMatchSnapshot();
+    });
+
+    it('1st box only is active when there are 39 spins', () => {
+        const sut = NumbersHitGameCounterOverlay({
+            transactionHistory: Array(39).fill(['x']),
+        });
+
+        const view = renderer.create(sut);
+
+        expect(view).toMatchSnapshot();
+    });
+
+    it('2nd box is active when there are 40 spins', () => {
+        const sut = NumbersHitGameCounterOverlay({
+            transactionHistory: Array(40).fill(['x']),
+        });
+
+        const view = renderer.create(sut);
+
+        expect(view).toMatchSnapshot();
+    });
+
+    it('2nd box is active when there are 49 spins', () => {
+        const sut = NumbersHitGameCounterOverlay({
+            transactionHistory: Array(49).fill(['x']),
+        });
+
+        const view = renderer.create(sut);
+
+        expect(view).toMatchSnapshot();
+    });
+
+    it('3rd box is active when there are 50 spins', () => {
+        const sut = NumbersHitGameCounterOverlay({
+            transactionHistory: Array(50).fill(['x']),
+        });
+
+        const view = renderer.create(sut);
+
+        expect(view).toMatchSnapshot();
+    });
+});
