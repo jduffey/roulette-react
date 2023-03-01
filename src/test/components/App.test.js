eca72b93c970f27a442ae8c31c561ec05265947c
Jed Duffey <jedduffey@gmail.com>
2023-03-01 15:28:19 -0500

    Reactivate App snapshot test

--- src/test/components/App.test.js
+++ src/test/components/App.test.js
@@ -1,24 +1,21 @@
-// import renderer from "react-test-renderer";
-// import { MemoryRouter } from "react-router-dom";
+import renderer from "react-test-renderer";
+import { MemoryRouter } from "react-router-dom";
 
-// import { App } from "../../components/App";
+import { App } from "../../components/App";
 
 describe("App", () => {
-    it('passes as a workaround to prevent CI/CD pipeline from failing when trying to make a websocket connection during tests', () => {
-        expect(true).toBe(true);
-    });
-    // it.each([
-    //     ["/"],
-    //     ["/roulette"],
-    //     ["/sic-bo"],
-    //     ["/balances"],
-    // ])("renders route \"%s\"", (path) => {
-    //     const view = renderer.create(
-    //         <MemoryRouter initialEntries={[path]} >
-    //             <App />
-    //         </MemoryRouter>,
-    //     );
+    it.each([
+        ["/"],
+        ["/roulette"],
+        ["/sic-bo"],
+        ["/balances"],
+    ])("renders route \"%s\"", (path) => {
+        const view = renderer.create(
+            <MemoryRouter initialEntries={[path]} >
+                <App />
+            </MemoryRouter>,
+        );
 
-    //     expect(view).toMatchSnapshot();
-    // });
+        expect(view).toMatchSnapshot();
+    });
 });
