04d9e95167269c53d7ca4930bf5a02119040be86
Jed Duffey <jedduffey@gmail.com>
2023-03-01 15:25:47 -0500

    Reactivate PlayerInfo snapshot test

--- src/test/components/roulette/PlayerInfo.test.js
+++ src/test/components/roulette/PlayerInfo.test.js
@@ -1,24 +1,20 @@
-// import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 
-// import { PlayerInfo } from '../../../components/roulette/PlayerInfo';
+import { PlayerInfo } from '../../../components/roulette/PlayerInfo';
 
 describe('PlayerInfo', () => {
-    it('passed as a workaround to prevent CI/CD pipeline from failing when trying to make a websocket connection during tests', () => {
-        expect(true).toBe(true);
+    it.each([
+        [undefined, 0],
+        [123456.354215, 56789], // TODO this test will fail if run in an environment with a different locale (e.g. using a period for thousands separator)
+    ])('renders when playerBalance is %s and totalBetAmount is %s', (playerBalance, totalBetAmount) => {
+        const sut =
+            <PlayerInfo
+                playerBalance={playerBalance}
+                totalBetAmount={totalBetAmount}
+            />;
+
+        const view = renderer.create(sut);
+
+        expect(view).toMatchSnapshot();
     });
-
-    // it.each([
-    //     ["foo", "bar"], // TODO shouldn't pass non-numeric values but still renders for now
-    //     [123456, 56789], // TODO this test will fail if run in an environment with a different locale (e.g. using a period for thousands separator)
-    // ])('renders when availableBalance is %s and totalBetAmount is %s', (availableBalance, totalBetAmount) => {
-    //     const sut =
-    //         <PlayerInfo
-    //             availableBalance={availableBalance}
-    //             totalBetAmount={totalBetAmount}
-    //         />;
-
-    //     const view = renderer.create(sut);
-
-    //     expect(view).toMatchSnapshot();
-    // });
 });
