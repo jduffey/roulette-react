0a167884219723d99dc899ff1a0d16fb56ad4217
Jed Duffey <jedduffey@gmail.com>
2023-03-06 22:10:18 -0500

    Test completions counter increments when a set has been completed

--- src/test/contracts/Roulette.js
+++ src/test/contracts/Roulette.js
@@ -276,5 +276,60 @@ describe("Roulette.sol", function () {
             });
         });
 
+        describe('getPlayerNumberCompletionSetsCounter', () => {
+            it("returns 0 when no sets have been completed", async function () {
+                const {
+                    RouletteContract,
+                    player1Address,
+                } = await loadFixture(fixtures);
+
+                const actual = await RouletteContract.getPlayerNumberCompletionSetsCounter(player1Address);
+
+                const expected = 0;
+                expect(actual).to.equal(expected);
+            });
+
+            it("returns 1 when one set has been completed", async function () {
+                const {
+                    MockRandomnessProviderContract,
+                    RouletteContract,
+                    player1Address,
+                } = await loadFixture(fixtures);
+
+                // Reminder: these are values supplied by the RandomnessProvider contract, not the spin results
+                const fakeRandomValues = [
+                    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+                    31, 32, 33, 34, 35, 36, 37
+                ];
+
+                async function setFakeRandomValueAndExecuteWager(fakeRandomValue) {
+                    return new Promise(resolve => {
+                        setTimeout(() => {
+                            MockRandomnessProviderContract.setFakeRandomValue(fakeRandomValue);
+                            RouletteContract.executeWager(player1Address);
+                            resolve();
+                        }, Math.random() * 1000);
+                    });
+                }
+
+                async function processTransactionPromises() {
+                    await Promise.all(fakeRandomValues.map(async fakeRandomValue => {
+                        await setFakeRandomValueAndExecuteWager(fakeRandomValue);
+                    }));
+                }
+
+                await processTransactionPromises();
+
+                const actual = await RouletteContract.getPlayerNumberCompletionSetsCounter(player1Address);
+
+                const expected = 1;
+                expect(actual).to.equal(expected);
+
+            });
+
+        });
+
     });
 });
