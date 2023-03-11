b4883327bd46f03701d1ad8ece38ddc5cabbe52a
Jed Duffey <jedduffey@gmail.com>
2023-03-11 16:17:31 -0500

    Test transferFrom() can send both less than and equal to the allowance

--- src/test/contracts/MyGameToken.js
+++ src/test/contracts/MyGameToken.js
@@ -240,7 +240,7 @@ describe("MyGameToken contract", () => {
             });
 
             describe("successful scenarios", () => {
-                it("can transfer the entire token balance (boundary condition)", async () => {
+                it("can transfer the entire token balance when the transfer amount is equal to the approval allowance (boundary condition)", async () => {
                     const { MyGameToken, acct0, acct1 } = await loadFixture(deployTokenFixture);
 
                     await MyGameToken.connect(acct0).deposit({ value: ethers.utils.parseEther("1") });
@@ -252,6 +252,21 @@ describe("MyGameToken contract", () => {
                     const actual = await MyGameToken.balanceOf(acct1.address);
 
                     const expected = ethers.utils.parseEther("100000");
+                    expect(actual).to.equal(expected);
+                });
+
+                it("can transfer the entire token balance when the transfer amount is less than the approval allowance (boundary condition)", async () => {
+                    const { MyGameToken, acct0, acct1 } = await loadFixture(deployTokenFixture);
+
+                    await MyGameToken.connect(acct0).deposit({ value: ethers.utils.parseEther("1") });
+
+                    await MyGameToken.connect(acct0).approve(acct1.address, ethers.utils.parseEther("100000"));
+
+                    await MyGameToken.connect(acct1).transferFrom(acct0.address, acct1.address, ethers.utils.parseEther("99999"));
+
+                    const actual = await MyGameToken.balanceOf(acct1.address);
+
+                    const expected = ethers.utils.parseEther("99999");
                     expect(actual).to.equal(expected);
                 });
 
