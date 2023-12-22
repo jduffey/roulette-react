04e6f4789f3ebb4b6ab450d082664456fc2e251a
Jed Duffey <jedduffey@gmail.com>
2023-12-22 16:08:23 -0500

    Hovering over game selectio box increases font size

--- src/components/NewHomeGameSelectionBox.jsx
+++ src/components/NewHomeGameSelectionBox.jsx
@@ -1,20 +1,44 @@
+import React, { useState } from 'react';
+
 export const NewHomeGameSelectionBox = ({
     gameName,
     gameImageUrl,
     disabled = false
 }) => {
+    const [hover, setHover] = useState(false);
+
+    const boxStyle = {
+        width: 250.33,
+        height: 247.42,
+        position: 'relative',
+        background: disabled ? 'grey' : 'white',
+        borderRadius: 15,
+        border: '4px black solid',
+        boxShadow: '4px 6px 0px 0px #000000',
+        opacity: disabled ? 0.5 : 1,
+        pointerEvents: disabled ? 'none' : 'auto',
+        cursor: disabled ? 'default' : 'pointer'
+    };
+
+    const textStyle = {
+        width: 210.66,
+        height: 40.56,
+        position: 'absolute',
+        left: 20,
+        top: 188,
+        textAlign: 'center',
+        color: 'black',
+        fontSize: hover ? '18px' : '16px',
+        fontFamily: 'Lexend Mega',
+        fontWeight: '700',
+        wordWrap: 'break-word'
+    };
+
     return (
-        <div style={{
-            width: 250.33,
-            height: 247.42,
-            position: 'relative',
-            background: disabled ? 'grey' : 'white',
-            borderRadius: 15,
-            border: '4px black solid',
-            boxShadow: '4px 6px 0px 0px #000000',
-            opacity: disabled ? 0.5 : 1,
-            // pointerEvents: disabled ? 'none' : 'auto'
-        }}
+        <div
+            style={boxStyle}
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
         >
             <img
                 style={{
@@ -29,22 +53,9 @@ export const NewHomeGameSelectionBox = ({
                 src={gameImageUrl}
                 alt={`placeholder for ${gameName}`}
             />
-            <div style={{
-                width: 210.66,
-                height: 40.56,
-                position: 'absolute',
-                left: 20,
-                top: 188,
-                textAlign: 'center',
-                color: 'black',
-                fontSize: 16,
-                fontFamily: 'Lexend Mega',
-                fontWeight: '700',
-                wordWrap: 'break-word'
-            }}
-            >
+            <div style={textStyle}>
                 {gameName}
             </div>
         </div>
-    )
-}
+    );
+};
