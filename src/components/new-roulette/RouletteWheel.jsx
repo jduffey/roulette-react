1627bf376850239b124f6a29e39de722dfa3de68
Jed Duffey <jed.duffey@augustwenty.com>
2024-01-05 18:30:16 -0500

    Place numbers in correct order with correct colors

--- src/components/new-roulette/RouletteWheel.jsx
+++ src/components/new-roulette/RouletteWheel.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react';
 
 const RouletteWheel = () => {
     const [spinning, setSpinning] = useState(false);
-    const numberOfSegments = 38;
-    const segments = Array.from({ length: numberOfSegments }, (_, i) => i + 1);
+
+    const rouletteNumbers = [0, 28, 9, 26, 30, 11, 7, 20, 32, 17, 5, 22, 34, 15, 3, 24, 36, 13, 1, 37, 27, 10, 25, 29, 12, 8, 19, 31, 18, 6, 21, 33, 16, 4, 23, 35, 14, 2];
 
     const startSpinning = () => {
         setSpinning(true);
@@ -11,39 +11,47 @@ const RouletteWheel = () => {
     };
 
     const displayNumber = (value) => {
+        if (value === 0) return '0';
         if (value === 37) return '00';
-        if (value === 38) return '0';
         return value;
     }
 
     const displayColor = (value) => {
-        if (value === 37 || value === 38) return 'green';
-        if (value % 2 === 0) return 'red';
-        return 'black';
-    }
+        if (value === 0 || value === 37) return 'green';
+
+        const redNumbers = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
+
+        return redNumbers.includes(value) ? 'red' : 'black';
+    };
+
+    const clipPathCompensation = 4.147;
+    const clipPath = `polygon(50% 100%, ${50 - clipPathCompensation}% 0%, ${50 + clipPathCompensation}% 0%)`;
 
     return (
         <div>
             <div className={`wheel ${spinning ? 'spin' : ''}`}>
-                {segments.map((number, index) => (
+                {rouletteNumbers.map((number, index) => (
                     <div
                         key={index}
                         className={`segment ${displayColor(number)}`}
                         style={{
-                            transform: `rotate(${360 / numberOfSegments * index}deg) translate(0%, 0%)`,
+                            transform: `rotate(${360 / rouletteNumbers.length * index}deg) translate(0%, 0%)`,
                             zIndex: index,
-                            clipPath: `polygon(50% 100%, 46% 0%, 54% 0%)`
+                            clipPath,
                         }}
                     >
                         <div className="number">
-                            {
-                                displayNumber(number)
-                            }
+                            {displayNumber(number)}
                         </div>
                     </div>
                 ))}
             </div>
-            <button onClick={startSpinning}>Spin</button>
+            <button
+                style={{
+                    width: '800px',
+                    height: '100px',
+                }}
+                onClick={startSpinning}>Spin</button>
         </div>
     );
 };
