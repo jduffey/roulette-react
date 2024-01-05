821c19374deb159d8363cf316da075b53b435ac1
Jed Duffey <jedduffey@gmail.com>
2024-01-05 12:35:40 -0500

    Add placeholders for Basic & Advanced Slots and Big Six (#33)

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 
 import { Layout } from "./Layout";
-
 import { Home } from "./Home";
-import { ChuckALuck } from "./chuck-a-luck/ChuckALuck";
-import { ThreeCardPoker } from "./three-card-poker/ThreeCardPoker";
-import { Roulette } from "./roulette/Roulette";
-import { NewRoulette } from "./new-roulette/NewRoulette";
-import { SicBo } from "./sic-bo/SicBo";
-import { Blackjack } from "./blackjack/Blackjack";
+
+import { AdvancedSlots } from "./advanced-slots/AdvancedSlots";
 import { Baccarat } from "./baccarat/Baccarat";
-import { Keno } from "./keno/Keno";
-import { SpacePoker } from "./space-poker/SpacePoker";
+import { BasicSlots } from "./basic-slots/BasicSlots";
+import { BigSix } from "./big-six/BigSix";
+import { Blackjack } from "./blackjack/Blackjack";
+import { ChuckALuck } from "./chuck-a-luck/ChuckALuck";
+import { CoinFlip } from "./coin-flip/CoinFlip";
 import { DiamondMiner } from "./diamond-miner/DiamondMiner";
 import { GiftTree } from "./gift-tree/GiftTree";
-import { CoinFlip } from "./coin-flip/CoinFlip";
+import { Keno } from "./keno/Keno";
+import { NewRoulette } from "./new-roulette/NewRoulette";
+import { Roulette } from "./roulette/Roulette";
+import { SicBo } from "./sic-bo/SicBo";
+import { SpacePoker } from "./space-poker/SpacePoker";
+import { ThreeCardPoker } from "./three-card-poker/ThreeCardPoker";
 
 import { Balances } from "./balances/Balances";
 
@@ -27,18 +30,21 @@ export function App() {
         <Routes>
             <Route path="/" element={elementInLayout(<Home />)} />
 
-            <Route path="/chuck-a-luck" element={elementInLayout(<ChuckALuck />)} />
-            <Route path="/three-card-poker" element={elementInLayout(<ThreeCardPoker />)} />
-            <Route path="/roulette" element={elementInLayout(<Roulette playerAddress={FIRST_PLAYER_ADDRESS} />)} />
-            <Route path="/new-roulette" element={elementInLayout(<NewRoulette />)} />
-            <Route path="/sic-bo" element={elementInLayout(<SicBo />)} />
-            <Route path="/blackjack" element={elementInLayout(<Blackjack />)} />
+            <Route path="/advanced-slots" element={elementInLayout(<AdvancedSlots />)} />
             <Route path="/baccarat" element={elementInLayout(<Baccarat />)} />
-            <Route path="/keno" element={elementInLayout(<Keno />)} />
-            <Route path="/space-poker" element={elementInLayout(<SpacePoker />)} />
+            <Route path="/basic-slots" element={elementInLayout(<BasicSlots />)} />
+            <Route path="/big-six" element={elementInLayout(<BigSix />)} />
+            <Route path="/blackjack" element={elementInLayout(<Blackjack />)} />
+            <Route path="/chuck-a-luck" element={elementInLayout(<ChuckALuck />)} />
+            <Route path="/coin-flip" element={elementInLayout(<CoinFlip />)} />
             <Route path="/diamond-miner" element={elementInLayout(<DiamondMiner />)} />
             <Route path="/gift-tree" element={elementInLayout(<GiftTree />)} />
-            <Route path="/coin-flip" element={elementInLayout(<CoinFlip />)} />
+            <Route path="/keno" element={elementInLayout(<Keno />)} />
+            <Route path="/new-roulette" element={elementInLayout(<NewRoulette />)} />
+            <Route path="/roulette" element={elementInLayout(<Roulette playerAddress={FIRST_PLAYER_ADDRESS} />)} />
+            <Route path="/sic-bo" element={elementInLayout(<SicBo />)} />
+            <Route path="/space-poker" element={elementInLayout(<SpacePoker />)} />
+            <Route path="/three-card-poker" element={elementInLayout(<ThreeCardPoker />)} />
 
             <Route path="/balances" element={elementInLayout(<Balances />)} />
         </Routes>
---
+++ src/components/basic-slots/BasicSlots.jsx
@@ -0,0 +1,13 @@
+export function BasicSlots() {
+    return (
+        <div
+            style={{
+                width: 1440,
+                height: 1024,
+                outline: '1px solid red',
+            }}
+        >
+            BASIC SLOTS
+        </div>
+    );
+}
