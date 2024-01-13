2f66d10d5aa174363f70159c0fed43e78652fb35
Jed Duffey <jedduffey@gmail.com>
2024-01-13 15:14:20 -0500

    Place chips centered on background bar

--- src/components/common/ChipSelection.jsx
+++ src/components/common/ChipSelection.jsx
@@ -6,24 +6,28 @@ import Chip100 from './chip-100.svg';
 import Chip500 from './chip-500.svg';
 
 function ChipSelection() {
+    const chips = [Chip1, Chip5, Chip10, Chip50, Chip100, Chip500];
+
+    const chipContainerStyle = {
+        width: '798px',
+        height: '142px',
+        background: '#CEFDCE',
+        borderRadius: '20px',
+        border: '6px black solid',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '0 16px 0 16px',
+    };
+
     return (
-        <>
-            <div style={{
-                width: '798px',
-                height: '142px',
-                background: '#CEFDCE',
-                borderRadius: 20,
-                border: '6px black solid',
-            }} />
-            <div>
-                <img src={Chip1} alt='chip-1' />
-                <img src={Chip5} alt='chip-5' />
-                <img src={Chip10} alt='chip-10' />
-                <img src={Chip50} alt='chip-50' />
-                <img src={Chip100} alt='chip-100' />
-                <img src={Chip500} alt='chip-500' />
+        <div>
+            <div style={chipContainerStyle}>
+                {chips.map((chip, index) => (
+                    <img key={index} src={chip} alt={`chip-${index + 1}`} />
+                ))}
             </div>
-        </>
+        </div>
     );
 }
 
