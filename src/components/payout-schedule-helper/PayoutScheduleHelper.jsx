6c55304231a00df18137e97cd6a0a71e9d626a06
Jed Duffey <jedduffey@gmail.com>
2024-01-24 04:51:34 -0500

    Add page title

--- src/components/payout-schedule-helper/PayoutScheduleHelper.jsx
+++ src/components/payout-schedule-helper/PayoutScheduleHelper.jsx
@@ -5,9 +5,20 @@ export function PayoutScheduleHelper() {
                 width: 1440,
                 height: 1024,
                 outline: '1px solid red',
-            }}
-        >
-            Payout Schedule Helper
+                backgroundColor: '#027eb1',
+            }}>
+            <div
+                style={{
+                    color: 'white',
+                    fontSize: 48,
+                    fontFamily: 'Lexend Mega',
+                    fontWeight: '700',
+                    wordWrap: 'break-word',
+                    width: '100%',
+                    textAlign: 'center',
+                }}>
+                PAYOUT SCHEDULE HELPER
+            </div>
         </div>
     );
 }
