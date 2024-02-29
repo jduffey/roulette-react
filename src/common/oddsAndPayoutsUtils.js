fe9a50c9feaebadeecfef8d74fdb66ea1a8dd370
Jed Duffey <jedduffey@gmail.com>
2024-02-29 15:37:55 -0700

    Throw if total possibilities is lte 0

--- src/common/oddsAndPayoutsUtils.js
+++ src/common/oddsAndPayoutsUtils.js
@@ -1,5 +1,9 @@
 const oddsOfWinning = (numberOfWinningPossibilities, totalNumberOfPossibilities) => {
-    return numberOfWinningPossibilities / totalNumberOfPossibilities;
+    if (totalNumberOfPossibilities <= 0) {
+        throw new Error('Total number of possibilities must be greater than 0.');
+    }
+    const result = numberOfWinningPossibilities / totalNumberOfPossibilities;
+    return result;
 };
 
 export { oddsOfWinning };
--- src/test/common/oddsAndPayoutsUtils.test.js
+++ src/test/common/oddsAndPayoutsUtils.test.js
@@ -9,4 +9,20 @@ describe('oddsAndPayoutsUtils', () => {
 
         expect(actual).toBe(expected);
     });
+
+    it('throws error if total number of possibilities is 0', () => {
+        const numberOfWinningPossibilities = Math.random() * 1000;
+        const totalNumberOfPossibilities = 0;
+
+        expect(() => oddsOfWinning(numberOfWinningPossibilities, totalNumberOfPossibilities))
+            .toThrow('Total number of possibilities must be greater than 0.');
+    });
+
+    it('throws error if total number of possibilities is negative', () => {
+        const numberOfWinningPossibilities = Math.random() * 1000;
+        const totalNumberOfPossibilities = -1;
+
+        expect(() => oddsOfWinning(numberOfWinningPossibilities, totalNumberOfPossibilities))
+            .toThrow('Total number of possibilities must be greater than 0.');
+    });
 });
