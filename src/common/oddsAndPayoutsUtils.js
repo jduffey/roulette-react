99325bd64a7069a3113a8b2d959539275594d4b5
Jed Duffey <jedduffey@gmail.com>
2024-03-01 11:03:06 -0700

    More tests

--- src/common/oddsAndPayoutsUtils.js
+++ src/common/oddsAndPayoutsUtils.js
@@ -1,6 +1,9 @@
 const oddsOfWinning = (numberOfWinningPossibilities, totalNumberOfPossibilities) => {
     if (totalNumberOfPossibilities <= 0) {
-        throw new Error('Total number of possibilities must be greater than 0.');
+        throw new Error('Total number of possibilities must be a positive number.');
+    }
+    if (numberOfWinningPossibilities <= 0) {
+        throw new Error('Number of winning possibilities must be a positive number.');
     }
     const result = numberOfWinningPossibilities / totalNumberOfPossibilities;
     return result;
--- src/test/common/oddsAndPayoutsUtils.test.js
+++ src/test/common/oddsAndPayoutsUtils.test.js
@@ -3,26 +3,31 @@ import { oddsOfWinning } from '../../common/oddsAndPayoutsUtils';
 describe('oddsAndPayoutsUtils', () => {
     it.each([
         [49, 100, 0.49],
-        [1, 2, 0.5]
+        [1, 2, 0.5],
     ])('calculates odds of winning returns odds', (numberOfWinningPossibilities, totalNumberOfPossibilities, expected) => {
         const actual = oddsOfWinning(numberOfWinningPossibilities, totalNumberOfPossibilities);
 
         expect(actual).toBe(expected);
     });
 
-    it('throws error if total number of possibilities is 0', () => {
-        const numberOfWinningPossibilities = Math.random() * 1000;
-        const totalNumberOfPossibilities = 0;
+    it.each([
+        [0],
+        [-1]
+    ])('throws if number of possibilities not a positive number', (numberOfWinningPossibilities) => {
+        const totalNumberOfPossibilities = Math.random() * 1000;
 
         expect(() => oddsOfWinning(numberOfWinningPossibilities, totalNumberOfPossibilities))
-            .toThrow('Total number of possibilities must be greater than 0.');
+            .toThrow('Number of winning possibilities must be a positive number.');
     });
 
-    it('throws error if total number of possibilities is negative', () => {
+    it.each([
+        [0],
+        [-1]
+    ])('throws if total number of possibilities is not a positive number', (totalNumberOfPossibilities) => {
         const numberOfWinningPossibilities = Math.random() * 1000;
-        const totalNumberOfPossibilities = -1;
 
-        expect(() => oddsOfWinning(numberOfWinningPossibilities, totalNumberOfPossibilities))
-            .toThrow('Total number of possibilities must be greater than 0.');
+        const action = () => oddsOfWinning(numberOfWinningPossibilities, totalNumberOfPossibilities);
+
+        expect(action).toThrow('Total number of possibilities must be a positive number.');
     });
 });
