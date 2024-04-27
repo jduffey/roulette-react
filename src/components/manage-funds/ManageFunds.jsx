ab1ef3d79091c2b44e5a5f5ef9e009f17d461d2f
Jed Duffey <jedduffey@gmail.com>
2024-04-27 10:49:14 -0400

    npm install @mui/material@next @emotion/react @emotion/styled

--- src/components/manage-funds/ManageFunds.jsx
+++ src/components/manage-funds/ManageFunds.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 export function ManageFunds() {
-    const [ethBalances, setEthBalances] = useState([]);
-    console.log(ethBalances);
+    // const [ethBalances, setEthBalances] = useState([]);
+    // console.log(ethBalances);
 
     return (
         <div
