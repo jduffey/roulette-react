294650ba4ec2e9b366091d1f13841fd899c0b268
Jed Duffey <jedduffey@gmail.com>
2024-04-27 14:13:39 -0400

    Styling; extracting styles to theme

--- src/components/manage-funds/ManageFunds.jsx
+++ src/components/manage-funds/ManageFunds.jsx
@@ -9,9 +9,6 @@ const playerTransactions = [
     { date: "2023-06-01", amount: "100 ETH", type: "Deposit" },
     { date: "2023-06-02", amount: "50 ETH", type: "Withdraw" },
     { date: "2023-06-03", amount: "200 ETH", type: "Deposit" },
-    { date: "2023-06-01", amount: "100 ETH", type: "Deposit" },
-    { date: "2023-06-02", amount: "50 ETH", type: "Withdraw" },
-    { date: "2023-06-03", amount: "200 ETH", type: "Deposit" },
 ];
 
 const houseTransactions = [
@@ -21,13 +18,23 @@ const houseTransactions = [
     { date: "2023-06-01", amount: "100 ETH", type: "Deposit" },
     { date: "2023-06-02", amount: "50 ETH", type: "Withdraw" },
     { date: "2023-06-03", amount: "200 ETH", type: "Deposit" },
+    { date: "2023-06-01", amount: "500 ETH", type: "Deposit" },
+    { date: "2023-06-02", amount: "100 ETH", type: "Withdraw" },
+    { date: "2023-06-03", amount: "300 ETH", type: "Deposit" },
+    { date: "2023-06-01", amount: "100 ETH", type: "Deposit" },
+    { date: "2023-06-02", amount: "50 ETH", type: "Withdraw" },
+    { date: "2023-06-03", amount: "200 ETH", type: "Deposit" },
 ];
 
-const tableTheme = createTheme({
-    typography: {
-        fontFamily: 'Lexend Mega, sans-serif',
-    },
+const transactionHistoryTableTheme = createTheme({
     components: {
+        MuiTableCell: {
+            styleOverrides: {
+                root: {
+                    fontFamily: 'Lexend Mega, sans-serif',
+                }
+            }
+        },
         MuiTableRow: {
             styleOverrides: {
                 root: {
@@ -37,6 +44,15 @@ const tableTheme = createTheme({
                     '&:nth-of-type(even)': {
                         backgroundColor: '#e0e0e0',
                     },
+                }
+            }
+        },
+        MuiTableContainer: {
+            styleOverrides: {
+                root: {
+                    width: '80%',
+                    borderRadius: '20px',
+                    border: '2px solid #000000',
                 }
             }
         }
@@ -80,38 +96,39 @@ const PlayerBox = () => (
         width: "50%",
         position: "relative"
     }}>
-        <Typography
-            variant="h4"
-            sx={{
-                fontFamily: "Lexend Mega, sans-serif",
-                fontWeight: "bold",
-                fontSize: "24px",
-                color: "#000000",
-                marginBottom: "8px",
-            }}
-        >
-            Player
-        </Typography>
-        <Typography
-            variant="h4"
-            sx={{
-                alignSelf: "flex-start",
-                fontFamily: "Lexend Mega, sans-serif",
-                fontWeight: "bold",
-                fontSize: "20px",
-                color: "#000000",
-                marginBottom: "8px",
-                paddingLeft: 'calc((100% - 80%)/2)',
-            }}
-        >
-            Transaction History
-        </Typography>
-        <ThemeProvider theme={tableTheme}>
-            <TableContainer
+        <Box sx={{
+            display: "flex",
+            width: "77.5%",
+            justifyContent: "space-between",
+            marginBottom: "8px",
+        }}>
+            <Typography
+                variant="h4"
                 sx={{
-                    height: "80%",
-                    width: "80%",
-                }}>
+                    fontFamily: "Lexend Mega, sans-serif",
+                    fontWeight: "bold",
+                    fontSize: "24px",
+                    color: "#000000",
+                }}
+            >
+                Player
+            </Typography>
+            <Typography
+                variant="h4"
+                sx={{
+                    fontFamily: "Lexend Mega, sans-serif",
+                    fontWeight: "bold",
+                    fontSize: "20px",
+                    color: "#000000",
+                }}
+            >
+                834.9332 ETH
+            </Typography>
+        </Box>
+        <ThemeProvider theme={transactionHistoryTableTheme}>
+            <TableContainer sx={{
+                height: "80%",
+            }}>
                 <Table>
                     <TableBody>
                         {playerTransactions.map((rowData, index) => (
@@ -125,7 +142,10 @@ const PlayerBox = () => (
                 </Table>
             </TableContainer>
         </ThemeProvider>
-        <Box sx={{ position: "absolute", bottom: "0px", outline: '0px solid red' }}>
+        <Box sx={{
+            position: "absolute",
+            bottom: "16px"
+        }}>
             <ButtonContainer children={[<DepositEthButton />, <WithdrawEthButton />]} />
         </Box>
     </Box>
@@ -138,38 +158,39 @@ const HouseBox = () => (
         alignItems: "center",
         width: "50%",
     }}>
-        <Typography
-            variant="h4"
-            sx={{
-                fontFamily: "Lexend Mega, sans-serif",
-                fontWeight: "bold",
-                fontSize: "24px",
-                color: "#000000",
-                marginBottom: "8px",
-            }}
-        >
-            House
-        </Typography>
-        <Typography
-            variant="h4"
-            sx={{
-                alignSelf: "flex-start",
-                fontFamily: "Lexend Mega, sans-serif",
-                fontWeight: "bold",
-                fontSize: "20px",
-                color: "#000000",
-                marginBottom: "8px",
-                paddingLeft: 'calc((100% - 80%)/2)',
-            }}
-        >
-            Transaction History
-        </Typography>
-        <ThemeProvider theme={tableTheme}>
-            <TableContainer
+        <Box sx={{
+            display: "flex",
+            width: "77.5%",
+            justifyContent: "space-between",
+            marginBottom: "8px",
+        }}>
+            <Typography
+                variant="h4"
                 sx={{
-                    height: "100%",
-                    width: "80%",
-                }}>
+                    fontFamily: "Lexend Mega, sans-serif",
+                    fontWeight: "bold",
+                    fontSize: "24px",
+                    color: "#000000",
+                }}
+            >
+                House
+            </Typography>
+            <Typography
+                variant="h4"
+                sx={{
+                    fontFamily: "Lexend Mega, sans-serif",
+                    fontWeight: "bold",
+                    fontSize: "20px",
+                    color: "#000000",
+                }}
+            >
+                9434.0046 ETH
+            </Typography>
+        </Box>
+        <ThemeProvider theme={transactionHistoryTableTheme}>
+            <TableContainer sx={{
+                height: "90%",
+            }}>
                 <Table>
                     <TableBody>
                         {houseTransactions.map((rowData, index) => (
